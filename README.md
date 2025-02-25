7f2ac1f11efa60b9e7e873f45737a0b324255fea
zjx <772109739@qq.com>
2025-02-25 21:11:51 +0800

    更新

--- README.md
+++ README.md
@@ -1,1 +1,116 @@
-# github-chinese
+# [GitHub 中文化插件][project-url]
+> 本项目源自: [52cik/github-hans](https://github.com/52cik/github-hans)
+
+  [![GitHub issues][issues-image]][issues-url]
+  [![GitHub stars][stars-image]][stars-url]
+  [![GitHub forks][forks-image]][forks-url]
+  [![license GPL-3.0][license-image]][license-url]
+  [![GreasyFork installs][greasyFork-image]][greasyFork-url]
+
+## 功能
+- 中文化 GitHub 菜单栏，标题，按钮等公共组件
+- 保留、完善正则功能
+- 参考: [k1995/github-i18n-plugin](https://github.com/k1995/github-i18n-plugin) 很多想法
+  + 对 “项目描述” 进行人机翻译
+  + 基于 JSON 格式的词条库文件
+- 繁体词库由[繁化姬项目][繁化姬项目] 简->繁
+
+## 安装
+1. 请先安装用户脚本管理器如: [Tampermonkey][Tampermonkey], [violentmonkey][violentmonkey] 等，支持的浏览器：Chrome, Microsoft Edge, Safari, Opera Next, 和 Firefox。
+2. 然后再点击链接之一，安装脚本即可。
+  -  [GitHub 中文化插件 - github 托管][main.user.js] 
+  -  [GitHub 中文化插件 - greasyfork 托管][main(greasyfork).user.js] 
+3. 刷新下页面，即可发现网站已中文化。
+
+> 测试平台: Win10 + Chrome + Tampermonkey, violentmonkey
+
+## 更新说明:
+
+#### 2022-05-12 15:44:00
+更新至 1.7.6(a)
+1. 新版 main.user.js 将使用 JSON 格式词条库文件，便于后期支持更多语言版本
+2. 原版 main.user.js --> main_zh-CN.user.js 仅用于日常维护中文词条库文件 locals_zh-CN.js
+3. 简中 JSON 格式词条库文件, 由 main_zh-CN.user.js 后台将 locals_zh-CN.js --> zh-CN.json
+4. 繁中 JSON 格式词条库文件, 由 [繁化姬项目][繁化姬项目]翻译将 zh-CN.json --> zh-TW.json
+
+#### 2022-05-12 13:53:46
+更新至 1.7.6
+1. 日常更新词库和忽略规则
+2. 添加手动开启/禁用正则翻译，添加切换菜单
+3. 优化翻译文本函数：避免已翻译词汇二次匹配，提高效率；局部翻译优先于全局
+
+#### 2022-02-26 12:36:14
+更新至 1.7.5
+
+#### 2022-01-21 13:34:06
+更新至 1.7.4
+
+#### 2021-12-26 12:01:11
+更新至 1.7.3
+
+#### 2021-12-01 09:04:58
+更新至 1.7.2
+
+#### 2021-11-23 10:51:22
+更新许可证为 [GPL-3.0][license-url] 希望大家依据许可证使用
+
+#### 2021-10-31 21:49:00
+正式发布 1.7.0 版本
+
+#### 2021-10-07 13:16:16
+
+原作者[楼教主](https://github.com/52cik/github-hans)已停止维护多年，且近年来 GitHub 页面结构的变化，导致原有的脚本无法正常工作。
+
+虽然 GitHub 在被微软售收购比较重视国际化，启动并基本完成了GitHub 文档的中文化。但是，关于 GitHub 页面的中文化暂时还没启动。
+
+对于，新手使用和高阶使用仍会存在一定的障碍。故，本人依据个人兴趣暂时进行了一定的修复和维护。
+
+本次维护基本恢复和保留大部分功能如：页面正则翻译（含日期的正则）。页面词条可能被我切得太碎不方便后期维护（先这样吧！）
+
+## 待办 (TODO)
+
+1. 添加 GitHub 名词解释，新手可能不太理解部分名词具体表达的意思，比如 `pull request`。
+2. 整理部分 git & [GitHub](https://github.com/) 学习资料, 帮助新手**更快**上手。
+3. **本人英文渣渣，翻译非常困难，急需大家 pr 共同翻译**
+
+## 贡献
+
+目前，已翻译大部分常用页面，欢迎补充完善，中文词条在`locals.js`中。大家在补充完善的过程，请遵循以下文档对相关术语进行翻译：
+
+相关概念及资料文档:
+
+1. [Pro Git 第二版 简体中文](https://www.gitbook.com/book/bingohuang/progit2/details)
+2. [Pro Git: 翻译约定](https://github.com/progit/progit2-zh/blob/master/TRANSLATION_NOTES.asc)
+3. [Git官方软件包的简体中文翻译](https://github.com/git/git/blob/master/po/zh_CN.po)
+4. [GitHub 词汇表官方译本](https://docs.github.com/cn/get-started/quickstart/github-glossary)
+
+## 预览
+
+  ![github-chinese][github-chinese]
+
+[project-url]: https://github.com/maboloshi/github-chinese "GitHub 中文化插件"
+
+[issues-url]: https://github.com/maboloshi/github-chinese/issues "议题"
+[issues-image]: https://img.shields.io/github/issues/maboloshi/github-chinese?style=flat-square&logo=github&label=Issue
+
+[stars-url]: https://github.com/maboloshi/github-chinese/stargazers "星标"
+[stars-image]: https://img.shields.io/github/stars/maboloshi/github-chinese?style=flat-square&logo=github&label=Star
+
+[forks-url]: https://github.com/maboloshi/github-chinese/network "复刻"
+[forks-image]: https://img.shields.io/github/forks/maboloshi/github-chinese?style=flat-square&logo=github&label=Fork
+
+[license-url]: https://opensource.org/licenses/GPL-3.0  "许可证"
+[license-image]: https://img.shields.io/github/license/maboloshi/github-chinese?style=flat-square&logo=github&label=License
+
+[greasyFork-url]: https://greasyfork.org/scripts/435208  "GreasyFork - GitHub 中文化插件"
+[greasyFork-image]: https://img.shields.io/badge/dynamic/json?style=flat-square&label=GreasyFork&query=total_installs&suffix=%20installs&url=https://greasyfork.org/scripts/435208.json
+
+[Tampermonkey]: http://tampermonkey.net/ "Tampermonkey"
+[violentmonkey]: https://violentmonkey.github.io/ "暴力猴"
+
+[main.user.js]: https://maboloshi.github.io/github-chinese/main.user.js "GitHub 中文化插件 - GitHub 托管"
+[main(greasyfork).user.js]: https://greasyfork.org/scripts/435208-github-%E4%B8%AD%E6%96%87%E5%8C%96%E6%8F%92%E4%BB%B6/code/GitHub%20%E4%B8%AD%E6%96%87%E5%8C%96%E6%8F%92%E4%BB%B6.user.js "GitHub 中文化插件 - GreasyFork 托管"
+
+[github-chinese]: https://raw.githubusercontent.com/maboloshi/github-chinese/gh-pages/preview/github-chinese.webp
+
+[繁化姬项目]: https://zhconvert.org/ "繁化姬项目"
---
+++ main.user.js
@@ -0,0 +1,424 @@
+// ==UserScript==
+// @name         GitHub 中文化插件
+// @name:zh-TW   GitHub 繁體中文化外掛
+// @namespace    https://github.com/maboloshi/github-chinese
+// @description  中文化 GitHub 界面的部分菜单及内容。原作者为楼教主(http://www.52cik.com/)。
+// @description:zh-TW  中文化 GitHub 介面的部分選單及內容。原作者為樓教主(http://www.52cik.com/)與 maboloshi(https://github.com/maboloshi/github-chinese)。
+// @copyright    2021, 沙漠之子 (https://maboloshi.github.io/Blog)
+// @icon         https://github.githubassets.com/pinned-octocat.svg
+// @version      1.7.6(a)
+// @author       沙漠之子
+// @license      GPL-3.0
+// @match        https://github.com/*
+// @match        https://gist.github.com/*
+// @resource     zh-CN https://github.com/maboloshi/github-chinese/raw/js2json/locales/zh-CN.json?v1.7.6
+// @resource     zh-TW https://github.com/maboloshi/github-chinese/raw/js2json/locales/zh-TW.json?v1.7.6
+// @run-at       document-end
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_notification
+// @connect      www.githubs.cn
+// ==/UserScript==
+
+(function (window, document, undefined) {
+    'use strict';
+
+    var enable_RegExp = GM_getValue("RegExp", 1);
+    const SUPPORT_LANG = ["zh-CN", "zh-TW"];
+    const lang = (navigator.language || navigator.userLanguage);
+    const locales = getLocales(lang);
+
+    // 要翻译的页面
+    var page = getPage();
+
+    transTitle(); // 页面标题翻译
+    translateBySelector(); // Selector 翻译
+    traverseNode(document.body); // 立即翻译页面
+    watchUpdate();
+
+    // 翻译描述
+    translateDesc(".f4.my-3"); //仓库简介翻译
+    translateDesc(".gist-content [itemprop='about']"); // Gist 简介翻译
+
+    function getLocales(lang) {
+        if(SUPPORT_LANG.includes(lang)) {
+            return JSON.parse(GM_getResourceText(lang));
+        }
+        return {
+            conf: {},
+            dict: {}
+        };
+    }
+
+    /**
+     * 监听节点变化, 触发和调用翻译函数
+     *
+     * 2021-10-07 11:28:30
+     * 使用原生API 代替 jQuery 的 `ajaxComplete`函数
+     */
+    function watchUpdate() {
+        const m =
+            window.MutationObserver ||
+            window.WebKitMutationObserver ||
+            window.MozMutationObserver;
+        var currentPath = location.pathname;
+        new m(function (mutations) {
+            /**
+             * 仅翻译变更部分 不在全局匹配
+             *
+             * 且仅监听:
+             *    1. 节点增加
+             *    2. 节点属性的变化
+             *
+             * 2021-10-10 15:24:49
+             * 遍历节点 函数 walk 需相应打2个补丁 适配
+             * */
+            if(location.pathname !== currentPath) {
+                currentPath = location.pathname;
+                page = getPage(); // 仅当,页面地址发生变化时运行
+            }
+            for(let mutation of mutations) { // for速度比forEach快
+                if (mutation.addedNodes || mutation.type === 'attributes') { // 仅当节点增加 或者属性更改
+                    traverseNode(mutation.target);
+                }
+            }
+        }).observe(document.body, {
+            subtree: true,
+            childList: true,
+            attributeFilter: ['value', 'placeholder', 'aria-label', 'data-confirm'], // 仅观察特定属性变化(试验测试阶段，有问题再恢复)
+        });
+
+        new m(function(mutations) {
+            transTitle();
+            translateBySelector(); // Selector 翻译 目前先跟随 url 即页面标题变化
+        }).observe(
+            document.querySelector('title'),
+            { childList: true }
+        );
+    }
+
+    /**
+     * 遍历节点
+     *
+     * @param {Element} node 节点
+     */
+    function traverseNode(node) {
+        // 跳过忽略
+        if (new RegExp(locales.conf.reIgnoreId).test(node.id) ||
+            new RegExp(locales.conf.reIgnoreClass).test(node.className) ||
+            new RegExp(locales.conf.reIgnoreTag).test(node.tagName) ||
+            (node.getAttribute && new RegExp(locales.conf.reIgnoreItemprop).test(node.getAttribute("itemprop")))
+           ) {
+            return;
+        }
+
+        var nodes = node.childNodes;
+
+        for (var i = 0, len = nodes.length; i <= len; i++) { // 遍历节点
+            var el = nodes[i] ? nodes[i] : node; //可能还要优化 该节点不存在子节点
+            // todo 1. 修复多属性翻译问题; 2. 添加事件翻译, 如论预览信息;
+
+            if (el.nodeType === Node.ELEMENT_NODE) { // 元素节点处理
+
+                // 元素节点属性翻译
+                if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') { // 输入框 按钮 文本域
+                    if (el.type === 'button' || el.type === 'submit' || el.type === 'reset') {
+                        if (el.hasAttribute('data-confirm')) { // 翻译 浏览器 提示对话框
+                            transElement(el, 'data-confirm', true);
+                        }
+                        transElement(el, 'value');
+                    } else {
+                        transElement(el, 'placeholder');
+                    }
+                } else if (el.tagName === 'BUTTON'){
+                    if (el.hasAttribute('aria-label') && /tooltipped/.test(el.className)) {
+                        transElement(el, 'aria-label', true); // 翻译 浏览器 提示对话框
+                    }
+                    if (el.hasAttribute('data-confirm')) {
+                        transElement(el, 'data-confirm', true); // 翻译 浏览器 提示对话框 ok
+                    }
+                    if (el.hasAttribute('data-confirm-text')) {
+                        transElement(el, 'data-confirm-text', true); // 翻译 浏览器 提示对话框 ok
+                    }
+                    if (el.hasAttribute('data-confirm-cancel-text')) {
+                        transElement(el, 'data-confirm-cancel-text', true); // 取消按钮 提醒
+                    }
+                    if (el.hasAttribute('cancel-confirm-text')) {
+                        transElement(el, 'cancel-confirm-text', true); // 取消按钮 提醒
+                    }
+                    if (el.hasAttribute('data-disable-with')) { // 按钮等待提示
+                        transElement(el.dataset, 'disableWith');
+                    }
+                } else if (el.tagName === 'OPTGROUP') { // 翻译 <optgroup> 的 label 属性
+                    transElement(el, 'label');
+                } else if (/tooltipped/.test(el.className)) { // 仅当 元素存在'tooltipped'样式 aria-label 才起效果
+                    transElement(el, 'aria-label', true); // 带提示的元素，类似 tooltip 效果的
+                }
+                if (el != node) {
+                    traverseNode(el); // 遍历子节点
+                }
+            } else if (el.nodeType === Node.TEXT_NODE) { // 文本节点翻译
+                if (el.length <= 500){ // 修复 许可证编辑框初始化载入内容被翻译
+                    transElement(el, 'data');
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取翻译页面
+     *
+     * 2021-10-07 11:48:50
+     * 参考 v2.0 中规则
+     */
+    function getPage() {
+        // 站点，如 gist, developer, help 等，默认主站是 github
+        const site = location.host.replace(/\.?github\.com$/, '') || 'github'; // 站点
+        const pathname = location.pathname; // 当前路径
+        const isLogin = /logged-in/.test(document.body.className); // 是否登录
+
+        // 用于确定 个人首页，组织首页，仓库页 然后做判断
+        const analyticsLocation = (document.getElementsByName('analytics-location')[0] || 0).content || '';
+        //const isProfile = analyticsLocation === '/<user-name>'; // 仅个人首页 其标签页识别不了 优先使用Class 过滤
+        // 如 maboloshi?tab=repositories 等
+        const isOrganization = /\/<org-login>/.test(analyticsLocation); // 组织页
+        const isRepository = /\/<user-name>\/<repo-name>/.test(analyticsLocation); // 仓库页
+
+        if (site === 'gist') { // Gist 站点
+            return 'gist';
+        }
+
+        if (pathname === '/' && site === 'github') { // github.com 首页
+            return isLogin ? 'page-dashboard' : 'homepage';
+        } //登录 或 未登录
+
+        // 仅个人首页 其标签页识别不了 优先使用 Class 过滤(/page-profile/)
+        // if (isProfile) { // 个人首页
+        //     return 'page-profile';
+        // }
+
+        if (isRepository) { // 仓库页
+            let t = pathname.match(new RegExp(locales.conf.rePagePathRepo));
+            return t ? locales.conf.redirect['repository/'+t[1]] ? locales.conf.redirect['repository/'+t[1]] : 'repository/'+t[1] : 'repository';
+        }
+
+        if (isOrganization) { // 组织页
+            let t = pathname.match(new RegExp(locales.conf.rePagePathOrg));
+            return t ? locales.conf.redirect['orgs/'+t[1]] ? locales.conf.redirect['orgs/'+t[1]] : 'orgs/'+t[1] : 'page-profile'; // orgs 重定向 page-profile
+        }
+
+        // 匹配 body 的 class
+        var page = document.body.className.match(new RegExp(locales.conf.rePageClass));
+
+        if (!page) { // 扩展 pathname 匹配
+            page = pathname.match(new RegExp(locales.conf.rePagePath));
+        }
+
+        return page ? locales.conf.redirect[page[1]] ? locales.conf.redirect[page[1]] : page[1] : false; // 取页面 key
+    }
+
+    /**
+     * 翻译页面标题
+     */
+    function transTitle() {
+        var title = translate(document.title, 'title');
+
+        if (!title) { // 无翻译则退出
+            return false;
+        }
+
+        document.title = title;
+    }
+
+
+    /**
+     * 翻译节点对应属性内容
+     *
+     * @param {object} el 对象
+     * @param {string} field 属性字段
+     * @param {boolean} isAttr 是否是 attr 属性
+     *
+     * @returns {boolean}
+     */
+    function transElement(el, field, isAttr=false) {
+        var transText; // 翻译后的文本
+
+        if (!isAttr) { // 非属性翻译
+            transText = translate(el[field], page);
+        } else {
+            transText = translate(el.getAttribute(field), page);
+        }
+
+        if (!transText) { // 无翻译则退出
+            return false;
+        }
+
+        // 替换翻译后的内容
+        if (!isAttr) {
+            el[field] = transText;
+        } else {
+            el.setAttribute(field, transText);
+        }
+    }
+
+
+    /**
+     * 翻译文本
+     *
+     * @param {string} text 待翻译字符串
+     * @param {string} page 页面字段
+     *
+     * @returns {string|boolean}
+     */
+    function translate(text, page) { // 翻译
+
+        if (!isNaN(text) || /^[\s]*[\u4e00-\u9fa5]|[\u4e00-\u9fa5][\s]*$/.test(text)) { ///^[\u4e00-\u9fa5]+.*$/.test(text)
+            return false;
+        } // 内容为空, 空白字符和或数字, 已翻译汉字 不翻译
+
+        var str;
+        var _key = text.trim(); // 去除首尾空格的 key
+        var _key_neat = _key
+            .replace(/\xa0/g, ' ') // 替换 &nbsp; 空格导致的 bug
+            .replace(/[\s]+/g, ' ') // 去除多余空白字符(空格 换行符)，(试验测试阶段，有问题再恢复)
+
+        if (page === 'title') {
+            return transPage('title', _key_neat);
+        } // 翻译网页标题
+
+        if (page) {
+            str = transPage(page, _key_neat); // 翻译已知页面 (局部优先)
+        } // 未知页面不翻译
+
+        if (str && str !== _key_neat) { // 已知页面翻译完成
+            return str;
+        }
+
+        str = transPage('pubilc', _key_neat); // 公共翻译
+
+        if (!str) {
+            return false;
+        } // 未知内容不翻译
+
+        return str;
+    }
+
+
+    /**
+     * 翻译页面内容
+     *
+     * @param {string} page 页面
+     * @param {string} key 待翻译内容
+     * @param {boolean} isRegexp 是否仅翻译正则部分
+     *
+     * @returns {string|boolean}
+     */
+    function transPage(page, key, isRegexp=false) {
+        var str; // 翻译结果
+
+        // 静态翻译
+        if (!isRegexp) {
+            str = locales.dict[page]['static'][key];
+            if (typeof str === 'string') {
+                return str;
+            }
+        }
+
+        // 正则翻译
+        if (enable_RegExp){
+            var res = locales.dict[page].regexp; // 正则数组
+            if (res) {
+                for (var i = 0, len = res.length; i < len; i++) {
+                    if (!res[i][1].startsWith("(function")) {
+                        str = key.replace(new RegExp(res[i][0]), res[i][1]);
+                    } else {
+                        str = key.replace(new RegExp(res[i][0]), new Function('return' + res[i][1])());
+                    }
+                    if (str !== key) {
+                        return str;
+                    }
+                }
+            }
+        }
+
+        return false; // 没有翻译条目
+    }
+
+    /**
+     * 翻译描述
+     *
+     * 2021-10-06 16:41:54
+     * 来自：k1995/github-i18n-plugin
+     * 改写为原生代码
+     */
+    function translateDesc(el) {
+        let element = document.querySelector(el);
+
+        if (!element) {
+            return false;
+        }
+
+        element.insertAdjacentHTML('afterend', "<a id='translate-me' href='#' style='color:rgb(27, 149, 224);font-size: small'>翻译</a>");
+        let translate_me = document.getElementById('translate-me')
+
+        translate_me.onclick = function() {
+            // get description text
+            const desc = element.textContent.trim();
+
+            if(!desc) {
+                return false;
+            }
+
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: `https://www.githubs.cn/translate?q=`+ encodeURIComponent(desc),
+                onload: function(res) {
+                    if (res.status === 200) {
+                         translate_me.style.display="none";
+                        // render result
+                        const text = res.responseText;
+                        element.insertAdjacentHTML('afterend', "<span style='font-size: small'>由 <a target='_blank' style='color:rgb(27, 149, 224);' href='https://www.githubs.cn'>GitHub中文社区</a> 翻译👇</span><br/>"+text);
+                    } else {
+                        alert("翻译失败");
+                    }
+                }
+            });
+        };
+    }
+
+    /**
+     * js原生选择器 翻译元素
+     *
+     * 2022-02-04 19:46:44
+     * 灵感参考自：k1995/github-i18n-plugin
+     */
+    function translateBySelector() {
+        var res = locales.dict.selector; // 数组
+        if (res) {
+            for (var i = 0, len = res.length; i < len; i++) {
+                let element = document.querySelector(res[i][0])
+                if (element) {
+                    element.textContent = res[i][1];
+                } else if (document.getElementsByClassName(res[i][0]).length != 0) {
+                    document.getElementsByClassName(res[i][0])[0].textContent = res[i][1];
+                }
+            }
+        }
+    }
+
+
+    GM_registerMenuCommand("正则切换", () => {
+        if (enable_RegExp){
+            GM_setValue("RegExp", 0);
+            GM_notification("已关闭正则功能");
+        } else {
+            GM_setValue("RegExp", 1);
+            GM_notification("已开启正则功能");
+            location.reload();
+        }
+    })
+
+})(window, document);
